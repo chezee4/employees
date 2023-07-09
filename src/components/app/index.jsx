554a1775bcc31363c8894b77acf6c0e06662e76c
chezee4 <vadimdufinec@gmail.com>
2023-07-09 20:43:58 +0300

    changed file names for .jsx file types to index.jsx

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,10 +1,10 @@
 import { Component } from "react";
 
-import AppInfo from "../app-info/app-info";
-import SearchPanel from "../search-panel/search-panel";
-import AppFilter from "../app-filter/app-filter";
-import EmployeesList from "../employees-list/employees-list";
-import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import AppInfo from "../app-info";
+import SearchPanel from "../search-panel";
+import AppFilter from "../app-filter";
+import EmployeesList from "../employees-list";
+import EmployeesAddForm from "../employees-add-form";
 
 import "./app.css";
 
